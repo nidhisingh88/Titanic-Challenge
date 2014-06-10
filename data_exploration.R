6e74eb8902b57087a7561427512cf977daa9782f
Nidhi Singh <nidhi@nidhi-ThinkPad-T430.(none)>
2014-06-10 19:07:17 +0200

    First commit

---
+++ data_exploration.R
@@ -0,0 +1,4 @@
+setwd('/home/nidhi/Courses//TUDelft-Data Analytics/Kaggle:Titanic problem')
+train_csv <- read.csv('train.csv')
+head(train_csv,n=2)
+Sweave("/home/nidhi/Courses//TUDelft-Data Analytics/Final Report/draft_1.Rnw")
